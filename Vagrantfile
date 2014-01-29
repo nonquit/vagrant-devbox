b22a52e6d7d871638df8e3f8db96a498ad2a4cee
sean <sean@torandu.ch>
2014-01-29 13:10:09 -0800

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,36 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.vm.box = "ubuntu"
+  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/precise/current/precise-server-cloudimg-amd64-vagrant-disk1.box"
+
+  config.vm.hostname = "devbox"
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  # config.vm.network :forwarded_port, guest: 80, host: 8080
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network :private_network, ip: "192.168.33.10"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network :public_network
+
+  config.ssh.forward_agent = true
+  config.vm.synced_folder "~/src", "/home/vagrant/src"
+
+  config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--memory", "2048"]
+  end
+
+  config.vm.provision "shell", path: "install.sh"
+end
---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+packages=" ack-grep build-essential git htop libssl-dev libxml2-dev
+libxslt1-dev python-dev python-pip rsync ruby1.9.1-dev tree vim "
+
+apt-get update
+apt-get -y install $packages
+
+update-alternatives --set editor /usr/bin/vim.basic
+update-alternatives --set ruby /usr/bin/ruby1.9.1
+update-alternatives --set gem /usr/bin/gem1.9.1
+
+gem install --no-ri --no-rdoc bundler
+
+pip install git-review
+pip install ipython
+pip install virtualenvwrapper
+
+debfile=chef-server_11.0.10-1.ubuntu.12.04_amd64.deb
+pkgs=/vagrant/pkgs
+mkdir -p $pkgs
+if [ ! -f $pkgs/$debfile ]; then
+    wget -P $pkgs \
+        https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/$debfile
+fi
+dpkg -i $pkgs/$debfile
+
+chef-server-ctl reconfigure
+chef-server-ctl test
+
+curl -L https://www.opscode.com/chef/install.sh | bash
+
+cat <<EOF > /etc/sudoers
+Defaults    env_reset
+Defaults    secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+root        ALL=(ALL:ALL) ALL
+vagrant     ALL=(ALL:ALL) NOPASSWD:ALL
+%admin      ALL=(ALL) NOPASSWD:ALL
+%sudo       ALL=(ALL:ALL) ALL
+EOF
+
+exit
+
+cat <<EOF > /home/vagrant/.ssh/config
+Host *
+ServerAliveInterval 300
+ServerAliveCountMax 12
+ForwardAgent yes
+StrictHostKeyChecking no
+EOF
+
+chown vagrant:vagrant /home/vagrant/.ssh/config
+chmod 600 /home/vagrant/.ssh/config
+
+cat <<EOF > /home/vagrant/.bash_aliases
+# .bash_aliases
+export EDITOR=vim
+export PAGER=less
+export LESS=-FRXi
+EOF
